6c727767ee0c80f9b76a24c5088c966388535c0c
cybnom <uriel.fiori@gmail.com>
2019-05-19 20:00:48 -0300

    transfered db operations to server-side

--- client/main.js
+++ client/main.js
@@ -12,10 +12,12 @@ import '../lib/collections';
 //views
 import './main.html';
 
+//username instead of emails
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY'
 });
 
+//helpers
 Template.main.helpers({
   title: function() {
     return 'QuickTodos';
@@ -26,18 +28,16 @@ Template.main.helpers({
   }
 });
 
+//events
 Template.main.events({
+  //insert todo
   "submit .add-todo": function(event) {
     event.preventDefault();
 
     const text = event.target.text.value;
     const time = event.target.time.value;
-    Todos.insert({
-      text,
-      time,
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
+
+    Meteor.call('todos.insert', text, time);
 
     event.target.text.value = '';
     event.target.time.value = '';
@@ -45,14 +45,12 @@ Template.main.events({
 });
 
 Template.todo.events({
+  //check todo item
   "click .toggle-checked": function(event) {
-    Todos.update(this._id, {
-      $set: {
-        checked: !this.checked
-      }
-    })
+    Meteor.call('todos.setChecked', this._id, !this.checked);
   },
+  //delete todo item
   "click .delete": function(event) {
-    Todos.remove(this._id);
+    Meteor.call('todos.remove', this._id)
   }
 });
--- server/main.js
+++ server/main.js
@@ -3,6 +3,30 @@ import {
 } from 'meteor/meteor';
 import '../lib/collections';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.methods({
+  //insert todo
+  "todos.insert": function(text, time) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Todos.insert({
+      text,
+      time,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  //check todo item
+  "todos.setChecked": function(id, setChecked) {
+    Todos.update(id, {
+      $set: {
+        checked: setChecked
+      }
+    });
+  },
+  //delete todo item
+  "todos.remove": function(id) {
+    Todos.remove(id);
+  }
 });
